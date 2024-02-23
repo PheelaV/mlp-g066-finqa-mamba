cb954e0ad082b0f8b595f956c35ef2b6ec2ff3b2
PheelaV <koudas1009@gmail.com>
2024-02-23 15:22:27 +0000

    creating environment for mamba_chat

--- mamba/mamba_chat_env.bash
+++ mamba/mamba_chat_env.bash
@@ -10,11 +10,21 @@ fi
 eval "$(conda shell.bash hook)"
 conda create -n $CONDA_ENV_NAME -y python=$PYTHON_VER 
 conda activate $CONDA_ENV_NAME
-conda install -y packaging pytorch==2.1.2 torchvision==0.16.2 torchaudio==2.1.2 pytorch-cuda=12.1 -c pytorch -c nvidia
-conda install -y transformers==4.35.0
-pip3 install --upgrade pip
-pip3 install accelerate==0.25.0 bitsandbytes==0.41.3 scipy==1.11.4
-pip3 install causal-conv1d==1.0.0 mamba-ssm==1.0.1
+# this step is just what worked for me, can be optimized
+conda install -y \
+    pytorch==2.1.1 \
+    torchvision==0.16.1 \
+    torchaudio==2.1.1 \
+    pytorch-cuda=12.1 \
+    -c pytorch -c nvidia
+pip install \
+    packaging torch==2.1.0 \
+    transformers==4.35.0 \
+    causal-conv1d==1.0.0 \
+    mamba-ssm==1.0.1 \
+    accelerate==0.25.0 \
+    bitsandbytes==0.41.3 \
+    scipy==1.11.4
 
 conda list
 echo "-----------------------"

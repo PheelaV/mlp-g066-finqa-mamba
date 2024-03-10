481830dd9ce30e83a4995034f003fcd73da03913
PheelaV <koudas1009@gmail.com>
2024-03-10 18:28:22 +0000

    wip

--- training/custom_training.py
+++ training/custom_training.py
@@ -8,9 +8,10 @@ from torch.nn.functional import cross_entropy
 from typing import Dict, Sequence
 import os
 from transformers import DataCollatorForSeq2Seq
-from transformers.trainer_pt_utils import nested_detach, smp_forward_only, smp_nested_concat
+from transformers.trainer_pt_utils import nested_detach
+# from transformers.trainer_pt_utils import smp_forward_only, smp_nested_concat
 
-from transformers.utils.import_utils import is_sagemaker_mp_enabled
+# from transformers.utils.import_utils import is_sagemaker_mp_enabled
 
 def _compute_loss(self, model, inputs, return_outputs=False):
     input_ids = inputs.pop("input_ids")
@@ -67,27 +68,6 @@ def _compute_loss(self, model, inputs, return_outputs=False):
 
 # because we are passing around the metadata for the length of the prompt, we need are not going to use labels
 # while labels are stil required for the computation somewhere and so this is an effective workaround
-# def _prediction_step(
-#     self,
-#     model: torch.nn.Module,
-#     inputs: Dict[str, Union[torch.Tensor, Any]],
-#     prediction_loss_only: bool,
-#     ignore_keys: Optional[List[str]] = None,
-# ) -> Tuple[Optional[torch.Tensor], Optional[torch.Tensor], Optional[torch.Tensor]]:
-#     with torch.no_grad():
-#         output = model(
-#             input_ids=inputs["input_ids"],
-#             #.to(model.device),
-#             labels=inputs["input_ids"]
-#             #.to(model.device),
-#         )
-            
-#     logits = nested_detach(output.logits)
-#     labels = None
-#     if prediction_loss_only:
-#         return (output.loss, logits, labels)
-
-
 def _prediction_step(
     self,
     model: torch.nn.Module,
@@ -95,103 +75,124 @@ def _prediction_step(
     prediction_loss_only: bool,
     ignore_keys: Optional[List[str]] = None,
 ) -> Tuple[Optional[torch.Tensor], Optional[torch.Tensor], Optional[torch.Tensor]]:
-    """
-    Perform an evaluation step on `model` using `inputs`.
-
-    Subclass and override to inject custom behavior.
-
-    Args:
-        model (`nn.Module`):
-            The model to evaluate.
-        inputs (`Dict[str, Union[torch.Tensor, Any]]`):
-            The inputs and targets of the model.
-
-            The dictionary will be unpacked before being fed to the model. Most models expect the targets under the
-            argument `labels`. Check your model's documentation for all accepted arguments.
-        prediction_loss_only (`bool`):
-            Whether or not to return the loss only.
-        ignore_keys (`List[str]`, *optional*):
-            A list of keys in the output of your model (if it is a dictionary) that should be ignored when
-            gathering predictions.
-
-    Return:
-        Tuple[Optional[torch.Tensor], Optional[torch.Tensor], Optional[torch.Tensor]]: A tuple with the loss,
-        logits and labels (each being optional).
-    """
-    has_labels = False if len(self.label_names) == 0 else all(inputs.get(k) is not None for k in self.label_names)
-    # For CLIP-like models capable of returning loss values.
-    # If `return_loss` is not specified or being `None` in `inputs`, we check if the default value of `return_loss`
-    # is `True` in `model.forward`.
-    return_loss = inputs.get("return_loss", None)
-    if return_loss is None:
-        return_loss = self.can_return_loss
-    loss_without_labels = True if len(self.label_names) == 0 and return_loss else False
-
-    inputs = self._prepare_inputs(inputs)
-    if ignore_keys is None:
-        if hasattr(self.model, "config"):
-            ignore_keys = getattr(self.model.config, "keys_to_ignore_at_inference", [])
-        else:
-            ignore_keys = []
-
-    # labels may be popped when computing the loss (label smoothing for instance) so we grab them first.
-    if has_labels or loss_without_labels:
-        labels = nested_detach(tuple(inputs.get(name) for name in self.label_names))
-        if len(labels) == 1:
-            labels = labels[0]
-    else:
-        labels = None
-
     with torch.no_grad():
-        if is_sagemaker_mp_enabled():
-            raw_outputs = smp_forward_only(model, inputs)
-            if has_labels or loss_without_labels:
-                if isinstance(raw_outputs, dict):
-                    loss_mb = raw_outputs["loss"]
-                    logits_mb = tuple(v for k, v in raw_outputs.items() if k not in ignore_keys + ["loss"])
-                else:
-                    loss_mb = raw_outputs[0]
-                    logits_mb = raw_outputs[1:]
-
-                loss = loss_mb.reduce_mean().detach().cpu()
-                logits = smp_nested_concat(logits_mb)
-            else:
-                loss = None
-                if isinstance(raw_outputs, dict):
-                    logits_mb = tuple(v for k, v in raw_outputs.items() if k not in ignore_keys)
-                else:
-                    logits_mb = raw_outputs
-                logits = smp_nested_concat(logits_mb)
-        else:
-            if has_labels or loss_without_labels:
-                with self.compute_loss_context_manager():
-                    loss, outputs = self.compute_loss(model, inputs, return_outputs=True)
-                loss = loss.mean().detach()
-
-                if isinstance(outputs, dict):
-                    logits = tuple(v for k, v in outputs.items() if k not in ignore_keys + ["loss"])
-                else:
-                    logits = outputs[1:]
-            else:
-                loss = None
-                with self.compute_loss_context_manager():
-                    outputs = model(**inputs)
-                if isinstance(outputs, dict):
-                    logits = tuple(v for k, v in outputs.items() if k not in ignore_keys)
-                else:
-                    logits = outputs
-                # TODO: this needs to be fixed and made cleaner later.
-                if self.args.past_index >= 0:
-                    self._past = outputs[self.args.past_index - 1]
-
+        output = model(
+            input_ids=inputs["input_ids"],
+            #.to(model.device),
+            labels=inputs["input_ids"]
+            #.to(model.device),
+        )
+            
+    logits = nested_detach(output.logits)
+    labels = None
     if prediction_loss_only:
-        return (loss, None, None)
+        return (output.loss, logits, labels)
 
-    logits = nested_detach(logits)
-    if len(logits) == 1:
-        logits = logits[0]
 
-    return (loss, logits, labels)
+# def _prediction_step(
+#     self,
+#     model: torch.nn.Module,
+#     inputs: Dict[str, Union[torch.Tensor, Any]],
+#     prediction_loss_only: bool,
+#     ignore_keys: Optional[List[str]] = None,
+# ) -> Tuple[Optional[torch.Tensor], Optional[torch.Tensor], Optional[torch.Tensor]]:
+#     """
+#     Perform an evaluation step on `model` using `inputs`.
+
+#     Subclass and override to inject custom behavior.
+
+#     Args:
+#         model (`nn.Module`):
+#             The model to evaluate.
+#         inputs (`Dict[str, Union[torch.Tensor, Any]]`):
+#             The inputs and targets of the model.
+
+#             The dictionary will be unpacked before being fed to the model. Most models expect the targets under the
+#             argument `labels`. Check your model's documentation for all accepted arguments.
+#         prediction_loss_only (`bool`):
+#             Whether or not to return the loss only.
+#         ignore_keys (`List[str]`, *optional*):
+#             A list of keys in the output of your model (if it is a dictionary) that should be ignored when
+#             gathering predictions.
+
+#     Return:
+#         Tuple[Optional[torch.Tensor], Optional[torch.Tensor], Optional[torch.Tensor]]: A tuple with the loss,
+#         logits and labels (each being optional).
+#     """
+#     has_labels = False if len(self.label_names) == 0 else all(inputs.get(k) is not None for k in self.label_names)
+#     # For CLIP-like models capable of returning loss values.
+#     # If `return_loss` is not specified or being `None` in `inputs`, we check if the default value of `return_loss`
+#     # is `True` in `model.forward`.
+#     return_loss = inputs.get("return_loss", None)
+#     if return_loss is None:
+#         return_loss = self.can_return_loss
+#     loss_without_labels = True if len(self.label_names) == 0 and return_loss else False
+
+#     inputs = self._prepare_inputs(inputs)
+#     if ignore_keys is None:
+#         if hasattr(self.model, "config"):
+#             ignore_keys = getattr(self.model.config, "keys_to_ignore_at_inference", [])
+#         else:
+#             ignore_keys = []
+
+#     # labels may be popped when computing the loss (label smoothing for instance) so we grab them first.
+#     if has_labels or loss_without_labels:
+#         labels = nested_detach(tuple(inputs.get(name) for name in self.label_names))
+#         if len(labels) == 1:
+#             labels = labels[0]
+#     else:
+#         labels = None
+
+#     with torch.no_grad():
+#         if is_sagemaker_mp_enabled():
+#             raw_outputs = smp_forward_only(model, inputs)
+#             if has_labels or loss_without_labels:
+#                 if isinstance(raw_outputs, dict):
+#                     loss_mb = raw_outputs["loss"]
+#                     logits_mb = tuple(v for k, v in raw_outputs.items() if k not in ignore_keys + ["loss"])
+#                 else:
+#                     loss_mb = raw_outputs[0]
+#                     logits_mb = raw_outputs[1:]
+
+#                 loss = loss_mb.reduce_mean().detach().cpu()
+#                 logits = smp_nested_concat(logits_mb)
+#             else:
+#                 loss = None
+#                 if isinstance(raw_outputs, dict):
+#                     logits_mb = tuple(v for k, v in raw_outputs.items() if k not in ignore_keys)
+#                 else:
+#                     logits_mb = raw_outputs
+#                 logits = smp_nested_concat(logits_mb)
+#         else:
+#             if has_labels or loss_without_labels:
+#                 with self.compute_loss_context_manager():
+#                     loss, outputs = self.compute_loss(model, inputs, return_outputs=True)
+#                 loss = loss.mean().detach()
+
+#                 if isinstance(outputs, dict):
+#                     logits = tuple(v for k, v in outputs.items() if k not in ignore_keys + ["loss"])
+#                 else:
+#                     logits = outputs[1:]
+#             else:
+#                 loss = None
+#                 with self.compute_loss_context_manager():
+#                     outputs = model(**inputs)
+#                 if isinstance(outputs, dict):
+#                     logits = tuple(v for k, v in outputs.items() if k not in ignore_keys)
+#                 else:
+#                     logits = outputs
+#                 # TODO: this needs to be fixed and made cleaner later.
+#                 if self.args.past_index >= 0:
+#                     self._past = outputs[self.args.past_index - 1]
+
+#     if prediction_loss_only:
+#         return (loss, None, None)
+
+#     logits = nested_detach(logits)
+#     if len(logits) == 1:
+#         logits = logits[0]
+
+#     return (loss, logits, labels)
 
 
 # AttributeError: 'NoneType' object has no attribute 'get'
@@ -235,9 +236,9 @@ class CustomSFTTrainer(SFTTrainer):
     def compute_loss(self, model, inputs, return_outputs=False):
         return _compute_loss(self, model, inputs, return_outputs=return_outputs)
     
-    def prediction_step(self, model: Module, inputs: Dict[str, torch.Tensor | Any], prediction_loss_only: bool, ignore_keys: List[str] | None = None) -> Tuple[torch.Tensor | None]:
-        # return super().prediction_step(model, inputs, prediction_loss_only, ignore_keys)
-        return _prediction_step(self, model, inputs, prediction_loss_only, ignore_keys)
+    # def prediction_step(self, model: Module, inputs: Dict[str, torch.Tensor | Any], prediction_loss_only: bool, ignore_keys: List[str] | None = None) -> Tuple[torch.Tensor | None]:
+    #     # return super().prediction_step(model, inputs, prediction_loss_only, ignore_keys)
+    #     return _prediction_step(self, model, inputs, prediction_loss_only, ignore_keys)
 
 
 class CustomTrainer(Trainer):

7557357806e71f67ba0998ea6ba0f0765bdb6a81
PheelaV <koudas1009@gmail.com>
2024-03-10 17:16:10 +0000

    wip

--- training/test_train.py
+++ training/test_train.py
@@ -11,7 +11,8 @@ model = AutoModelForCausalLM.from_pretrained(model_id)
 dataset = load_dataset("Abirate/english_quotes", split="train[:10%]")
 training_args = TrainingArguments(
     output_dir="./results",
-    num_train_epochs=3,
+    # num_train_epochs=3,
+    num_train_epochs=1,
     per_device_train_batch_size=4,
     logging_dir='./logs',
     logging_steps=10,

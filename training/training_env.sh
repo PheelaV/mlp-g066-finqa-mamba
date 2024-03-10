881ea6058d9ccc5220b168afcac5fb6c4fed5d9e
PheelaV <koudas1009@gmail.com>
2024-03-10 20:29:02 +0000

    training env update

--- training/training_env.sh
+++ training/training_env.sh
@@ -11,21 +11,31 @@ eval "$(conda shell.bash hook)"
 conda create -n $CONDA_ENV_NAME -y python=$PYTHON_VER 
 conda activate $CONDA_ENV_NAME
 # linux:
-# conda install -y \
-#     pytorch==2.1.1 \
-#     torchvision==0.16.1 \
-#     torchaudio==2.1.1 \
-#     pytorch-cuda=12.1 \
-#     -c pytorch -c nvidia
+conda install -y \
+    pytorch==2.1.1 \
+    torchvision==0.16.1 \
+    torchaudio==2.1.1 \
+    pytorch-cuda=12.1 \
+    -c pytorch -c nvidia \
+    pytest pandas numpy tqdm ipykernel ipywidgets packaging nbconvert
 
 # arm mac:
-conda install -y pytorch::pytorch torchvision torchaudio -c pytorch
+# conda install -y pytorch::pytorch torchvision torchaudio -c pytorch
 
 # the usual
-conda install -y transformers datasets pytest pandas numpy tqdm ipykernel ipywidgets packaging nbconvert
+conda install -y pytest pandas numpy tqdm ipykernel ipywidgets packaging nbconvert
+
+# 
+# for mamba 
+# You need to install transformers from main until transformers=4.39.0 is released.
+# https://huggingface.co/state-spaces/mamba-2.8b-hf 
+pip install git+https://github.com/huggingface/transformers@main
 
 pip install -U datasets trl wandb peft
 
 conda list
 echo "-----------------------"
 echo "DONE"
+
+
+# pip install deepspeed

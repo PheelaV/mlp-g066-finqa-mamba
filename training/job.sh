faeb8ba73632a99340c746a27a5a1dd7f7ccaca6
PheelaV <koudas1009@gmail.com>
2024-03-11 04:33:58 +0000

    job

---
+++ training/job.sh
@@ -0,0 +1,53 @@
+#!/bin/sh
+#SBATCH -N 1	  # nodes requested
+#SBATCH -n 1	  # tasks requested
+#SBATCH --partition=Teach-Standard
+#SBATCH --gres=gpu:a6000:2
+#SBATCH 
+#SBATCH --mem=12000  # memory in Mb
+#SBATCH --time=0-08:00:00
+
+export CUDA_HOME=/opt/cuda-12.2.1/
+
+# export CUDNN_HOME=/opt/cuDNN-7.0/
+
+export STUDENT_ID=$(whoami)
+
+# export LD_LIBRARY_PATH=${CUDNN_HOME}/lib64:${CUDA_HOME}/lib64:$LD_LIBRARY_PATH
+
+export LIBRARY_PATH=${CUDNN_HOME}/lib64:$LIBRARY_PATH
+
+# export CPATH=${CUDNN_HOME}/include:$CPATH
+
+export PATH=${CUDA_HOME}/bin:${PATH}
+
+export PYTHON_PATH=$PATH
+
+mkdir -p /disk/scratch/${STUDENT_ID}
+
+
+export TMPDIR=/disk/scratch/${STUDENT_ID}/
+export TMP=/disk/scratch/${STUDENT_ID}/
+
+mkdir -p ${TMP}/datasets/
+export DATASET_DIR=${TMP}/datasets/
+
+export HF_DATASETS_CACHE=$DATASET_DIR
+
+# df--gres=gpu:a6000:2
+
+source /home/${STUDENT_ID}/miniconda3/bin/activate mlp_g066_training
+cd ..
+
+cd ~/repos/mlp-g066-finqa-mamba/training
+accelerate launch train.py \
+--run_name mamba_s_mt_0-1\ 
+--dataset sentiment-train,headline,finred*3,ner*15 \
+--max_length 512 \
+--config config_mt.json \
+--distributed 1 \
+--eval_accumulation_steps 8 \
+--prompt_loss_weight 0.1 \
+--base_model mamba-small \
+--output_dir $TMP \
+>train.log 2>&1 &

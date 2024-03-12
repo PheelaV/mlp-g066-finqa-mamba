ba8194988dafacf75752ae9e7ad08e36858d9466
PheelaV <koudas1009@gmail.com>
2024-03-12 23:16:37 +0000

    upgrade

--- training/test_train.py
+++ training/test_train.py
@@ -4,7 +4,6 @@ from collections import namedtuple
 import os
 import argparse
 
-from custom_training import CustomSFTTrainer as SFTTrainer
 import custom_training
 from peft import LoraConfig
 from transformers import AutoTokenizer, AutoModelForCausalLM, TrainingArguments
@@ -15,7 +14,15 @@ parser.add_argument("--model_size", type=str)
 parser.add_argument("--lora", type=bool, default=False)
 parser.add_argument("--model_type", type=str)
 parser.add_argument("--seq_len", type=int, default=512)
+parser.add_argument("--reference", type=argparse.BooleanOptionalAction)
+
 args = parser.parse_args()
+
+if args.reference:
+    from trl import SFTTrainer
+else:
+    from custom_training import CustomSFTTrainer as SFTTrainer
+
 if args.model_type == "pythia":
     target_modules = ["query_key_value"]
     if args.model_size == "l":
@@ -62,7 +69,7 @@ dataset_args = dataset_args(
 )
 # dataset_args = dataset_args("convfinqa", 512, False, None, "default", None)
 
-dataset = utils.get_dataset(args=dataset_args, tokenizer=tokenizer, return_text=False)
+dataset = utils.get_dataset(args=dataset_args, tokenizer=tokenizer, return_text=args.reference)
 
 del tokenizer
 training_args = TrainingArguments(
@@ -95,18 +102,30 @@ if tokenizer.pad_token_id is None:
     tokenizer.pad_token = tokenizer.eos_token
     tokenizer.pad_token_id = tokenizer.eos_token_id
 
-trainer = SFTTrainer(
-    model=model,
-    args=training_args,
-    peft_config=lora_config if args.lora else None,
-    train_dataset=dataset["train"],
-    eval_dataset=dataset["test"],
-    max_seq_length=args.seq_len,
-    # dataset_text_field="input_ids", # not nescessary as we are using a customized trainer
-    data_collator=custom_training.CustomDataCollatorSeq2Seq(
-        tokenizer, padding=True, prompt_loss_weight=complete_args["prompt_loss_weight"]
-    ),
-    tokenized_datasets=True,
-    prompt_loss_weight=complete_args["prompt_loss_weight"],
-)
+if args.reference:
+    trainer = SFTTrainer(
+        model=model,
+        tokenizer=tokenizer,
+        args=training_args,
+        peft_config=lora_config if args.lora else None,
+        train_dataset=dataset["train"],
+        eval_dataset=dataset["test"],
+        dataset_text_field="input_ids",
+        max_seq_length=args.seq_len,
+    )
+else:
+    trainer = SFTTrainer(
+        model=model,
+        args=training_args,
+        peft_config=lora_config if args.lora else None,
+        train_dataset=dataset["train"],
+        eval_dataset=dataset["test"],
+        max_seq_length=args.seq_len,
+        # dataset_text_field="input_ids", # not nescessary as we are using a customized trainer
+        data_collator=custom_training.CustomDataCollatorSeq2Seq(
+            tokenizer, padding=True, prompt_loss_weight=complete_args["prompt_loss_weight"]
+        ),
+        tokenized_datasets=True,
+        prompt_loss_weight=complete_args["prompt_loss_weight"],
+    )
 trainer.train()

56fd6b960c974a2fac5bc206d295997dcb3b1001
PheelaV <koudas1009@gmail.com>
2024-03-15 00:50:15 +0000

    data fix

--- FinGPT-benchmarks/data/download_data.py
+++ FinGPT-benchmarks/data/download_data.py
@@ -8,7 +8,7 @@ import pathlib
 # just so we can execute this from any directory
 local_file_folder = pathlib.Path(__file__).parent.absolute()
 
-file_paths = [os.path.join(local_file_folder, path) for path in ('twitter-financial-news-sentiment', 'news_with_gpt_instructions', 'financial_phrasebank-sentences_50agree', 'fiqa-2018')]
+file_paths = [os.path.join(local_file_folder, path) for path in ("twitter-financial-news-sentiment", "news_with_gpt_instructions", "financial_phrasebank-sentences_50agree", "fiqa-2018")]
 
 
 for file_path in file_paths:
@@ -16,14 +16,14 @@ for file_path in file_paths:
         shutil.rmtree(file_path)
         
 # Load and save the datasets
-dataset = load_dataset('pauri32/fiqa-2018')
-dataset.save_to_disk('fiqa-2018')
+dataset = load_dataset("pauri32/fiqa-2018")
+dataset.save_to_disk(os.path.join(local_file_folder, "fiqa-2018"))
 
-dataset = load_dataset('financial_phrasebank', 'sentences_50agree')
-dataset.save_to_disk('financial_phrasebank-sentences_50agree')
+dataset = load_dataset("financial_phrasebank", "sentences_50agree")
+dataset.save_to_disk(os.path.join(local_file_folder, "financial_phrasebank-sentences_50agree"))
 
-dataset = load_dataset('zeroshot/twitter-financial-news-sentiment')
-dataset.save_to_disk('twitter-financial-news-sentiment')
+dataset = load_dataset("zeroshot/twitter-financial-news-sentiment")
+dataset.save_to_disk(os.path.join(local_file_folder, "twitter-financial-news-sentiment"))
 
-dataset = load_dataset('oliverwang15/news_with_gpt_instructions')
-dataset.save_to_disk('news_with_gpt_instructions')
+dataset = load_dataset("oliverwang15/news_with_gpt_instructions")
+dataset.save_to_disk(os.path.join(local_file_folder, "news_with_gpt_instructions"))
